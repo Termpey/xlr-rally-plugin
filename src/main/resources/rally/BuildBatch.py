b957a5f53caa37cc0a2fb9051155eb363d29f751
Termpey <cjt032000@gmail.com>
2020-01-15 14:52:03 -0600

    Foundation for Batch JSON building

--- src/main/resources/rally/BuildBatch.py
+++ src/main/resources/rally/BuildBatch.py
@@ -1,7 +1,37 @@
 
 class BuildBatch:
+    
+    funcDictionary = {"\"Description\":": "\"\"", "\"Notes\":": "\"\"", "\"c_AcceptanceCriteria\":":"\"\"","\"PlannedStartDate\":": "\"\""}
 
-    def __init__(buildArray):
-        batch = """{"Batch":{[ "Entry":{"""
-        batchCap = """}]}}"""
+    def __init__(self, path):
+        self.batch = """{"Batch":[{"Entry":{ "Path": """ + path
+        self.type = "\"feature\": {" if "feature" in path else "\"hierarchicalrequirement\": {"
+
+    def buildJson(self):
+
+        self.batch += ", \"Method\": \"POST\", \"Body\": { " + self.type
+
+        for i in BuildBatch.funcDictionary:
+            
+            self.batch += i + BuildBatch.funcDictionary.get(i) + ","
+
+        batchCap = ""
+        
+        for c in self.batch:
+
+            if c == "{":
+                batchCap = "}" + batchCap
+            elif c =="[":
+                batchCap = "]"+ batchCap
+
+        self.batch = self.batch[:-1]
+
+        print self.batch + batchCap
+
+        return ""
+
+test = BuildBatch("\"/hierarchicalrequirement/\"")
+
+test.buildJson()
+
         
